8b84f2c01c5c2fec4fdd085263710ff41a5852b9
Da-vid-Lin <DavidLin771@outlook.com>
2026-03-29 05:12:12 +0100

    - Testing methods for calling API

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import { useEffect } from 'react'
+import { fetchCurrentWeather } from './services/weatherAPI'
+
 export default function App() {
-  return <h1>My React App</h1>
+
+	useEffect(() => {
+    fetchCurrentWeather(51.5073219, -0.1276474)
+      	.then(data => console.log('Weather data:', data))
+      	.catch(err => console.error('Error fetching weather:', err))
+  	}, [])
+
+
+	return (
+		<h1>My React App</h1>
+	)
 }
