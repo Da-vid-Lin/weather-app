5ea38cdb2819ca6395d8d23f45a6d770cb1b808f
Da-vid-Lin <DavidLin771@outlook.com>
2026-03-29 23:05:55 +0100

    - Added our different pages for the website

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import SearchLocation from '../components/SearchWeather.jsx'
+
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <SearchLocation />
+        </div>
+    )
+}
---
+++ src/pages/WeatherPage.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react'
+import { fetchCurrentWeather } from '../services/weatherAPI'
+import { fetchAirQuality } from '../services/weatherAPI'
+import { searchCity } from '../services/geocodingAPI'
+import { useWeather } from '../contexts/weatherContext'
+import { setLocationA } from '../contexts/weatherActions'
+import LocationCard from '../components/LocationCard'
+
+export default function WeatherPage() {
+    const { state, dispatch } = useWeather()
+    const [currentWeather, setCurrentWeather] = useState(null)
+    const [currentQuality, setCurrentQuality] = useState(null)
+    const [currentName, setCurrentName] = useState(null)
+
+    // Using api to get cords and weather info
+    useEffect(() => {
+        if (!state.locationA) { return }
+
+        async function loadWeather() {
+            const cities = await searchCity(state.locationA)
+            console.log('Cities found:', cities)
+
+            const city = cities[0]
+            const currentWeather = await fetchCurrentWeather(city.lat, city.lon)
+            console.log('Weather fetched:', currentWeather)
+
+            const currentQuality = await fetchAirQuality(city.lat, city.lon)
+            console.log('Quality fetched:', currentQuality)
+
+            setCurrentWeather(currentWeather)
+            setCurrentQuality(currentQuality)
+            setCurrentName(city.name)
+        }
+
+        loadWeather()
+    }, [state.locationA])
+
+    // Displaying the weather information
+    return (
+        <div>
+            <LocationCard weatherData={currentWeather} weatherQuality={currentQuality} locationName={currentName}/>
+        </div>
+    )
+}
