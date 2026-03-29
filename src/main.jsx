e4a502901eb3d5e3a09bbdcc113381bfbdbd8a54
Da-vid-Lin <DavidLin771@outlook.com>
2026-03-29 05:09:59 +0100

    - Added my wife

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
+import { SettingsProvider } from './contexts/SettingsContext.jsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const img = new Image()
+img.src = '/Salter.png'
+ 
+img.onload = () => {
+	createRoot(document.getElementById('root')).render(
+		<StrictMode>
+			<SettingsProvider>
+				<App />
+			</SettingsProvider>
+		</StrictMode>,
+	)
+}
+
+img.onerror = () => {
+  document.body.innerHTML = "<h1>Error: Missing Saber Alter</h1>"
+  console.log("Error: Missing Saber Alter")
+  window.stop()
+}
