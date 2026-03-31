0e6e8f3de00d4a98b0cb1bdfe043ec7faf688e07
Da-vid-Lin <DavidLin771@outlook.com>
2026-03-31 02:00:23 +0100

    - Combined GearSuggest and CyclingAdvice to one card

--- src/components/Cards/CyclingAdviceCard.jsx
+++ src/components/Cards/CyclingAdviceCard.jsx
@@ -1,4 +1,8 @@
-import { getCyclingScore } from "../../utils/cycleRating"
+// Displays the cycling advice and gear suggestions based on the weather data
+
+import { getCyclingScore } from "../../utils/cyclingScore"
+import { getGearSuggest } from "../../utils/gearSuggest"
+
 import "../../styles/LocationCard.css";
 
 import perfectBikeImg from '../../assets/ratingIcons/star.png';
@@ -8,23 +12,36 @@ import hazardBikeImg from '../../assets/images/Warning.png';
 import stopBikeImg from '../../assets/ratingIcons/no-bike.png';
 
 const adviceImages = {
-  'Perfect cycling conditions': perfectBikeImg,
-  'Decent cycling conditions': decentBikeImg,
-  'Bad cycling conditions': badBikeImg,
-  'Hazardous cycling conditions': hazardBikeImg,
-  'Not recommended cycling': stopBikeImg
+  	'Perfect cycling conditions': perfectBikeImg,
+  	'Decent cycling conditions': decentBikeImg,
+  	'Bad cycling conditions': badBikeImg,
+  	'Hazardous cycling conditions': hazardBikeImg,
+  	'Not recommended cycling': stopBikeImg
 };
 
 export default function CyclingAdvice({ weatherData, weatherQuality}){
-  const cyclingMessage = getCyclingScore(weatherData, weatherQuality)
-  const displayImage = adviceImages[cyclingMessage[0]];
+  	const cyclingMessage = getCyclingScore(weatherData, weatherQuality)
+	const gearMessage = getGearSuggest(weatherData)
+  	const displayImage = adviceImages[cyclingMessage[0]];
 
-
-  return(
-    <div className="cycling-advice-card">
-        <h3 className="advice-rating">{cyclingMessage[1]+'%'}</h3>
-        <img src={displayImage} alt={cyclingMessage[0]} className="cycling-image" />
-        <p className="advice-message">{cyclingMessage[0]}</p>
-    </div>
-  )
+  	return(
+            <div className="stat-breakdown">
+				<div className="stat-sub">
+					<span className="stat-sub-label">Cycling Score</span>
+        			<span className="stat-sub-value">{cyclingMessage[1]}%</span>
+				</div>
+				<div className="stat-sub">
+					<span className="stat-sub-label">Advice</span>
+        			<span className="stat-sub-value">{cyclingMessage[0]}</span>
+				</div>
+				<div className="stat-sub">
+					<span className="stat-sub-label">Gear</span>
+        			<span className="stat-sub-value">{gearMessage}</span>
+				</div>
+			</div>
+  	)
 }
+
+//<div className="cycling-advice-card">
+    //<img src={displayImage} alt={cyclingMessage[0]} className="cycling-image" />
+//</div>
