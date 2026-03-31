507e93b89694a5014d3d569504ed9f54313747fb
Da-vid-Lin <DavidLin771@outlook.com>
2026-03-31 03:07:08 +0100

    - Fixed 2 space indentation instead of 4

--- src/components/SelectLocation.jsx
+++ src/components/SelectLocation.jsx
@@ -7,46 +7,46 @@ import LocationCapsule from './LocationCapsule';
 import { useSettings } from '../contexts/settingsContext';
 
 export default function SelectLocation() {
-  const { state } = useWeather();
-  const { state: settingsState } = useSettings();
+	const { state } = useWeather();
+	const { state: settingsState } = useSettings();
 
-  return (
-    <div className="app-wrapper">
-      <div className="mobile-container">
-        <div className="list-section">
-          <h2 className="section-title">Home</h2>
-          <div className="action-card">
-            <Link to="/search/home" className="icon-circle">
-              <img src={Plus} alt="Set Home" />
-            </Link>
-            <Link to="/weather" className="card-text">
-              {state.locationA || 'Select a Location'}
-            </Link>
-          </div>
-        </div>
+	return (
+		<div className="app-wrapper">
+			<div className="mobile-container">
+				<div className="list-section">
+					<h2 className="section-title">Home</h2>
+					<div className="action-card">
+						<Link to="/search/home" className="icon-circle">
+							<img src={Plus} alt="Set Home" />
+						</Link>
+						<Link to="/weather" className="card-text">
+							{state.locationA || 'Select a Location'}
+						</Link>
+					</div>
+				</div>
 
-        <div className="list-section">
-          <h2 className="section-title">Destination</h2>
-          <div className="action-card">
-            <Link to="/search/dest" className="icon-circle">
-              <img src={Plus} alt="Set Destination" />
-            </Link>
-            <Link to="/weather" className="card-text">
-              {state.locationB || 'Select a Location'}
-            </Link>
-          </div>
-        </div>
+				<div className="list-section">
+					<h2 className="section-title">Destination</h2>
+					<div className="action-card">
+						<Link to="/search/dest" className="icon-circle">
+							<img src={Plus} alt="Set Destination" />
+						</Link>
+						<Link to="/weather" className="card-text">
+							{state.locationB || 'Select a Location'}
+						</Link>
+					</div>
+				</div>
 
-        {settingsState.recentDestinations.length > 0 && (
-          <div className='recents'>
-            <h2 className="section-title">Recent Searches</h2>
-            {settingsState.recentDestinations.map((city) => (
-              <LocationCapsule key={city} city={city} />
-            ))}
-          </div>
-        )}
+				{settingsState.recentDestinations.length > 0 && (
+					<div className='recents'>
+						<h2 className="section-title">Recent Searches</h2>
+						{settingsState.recentDestinations.map((city) => (
+							<LocationCapsule key={city} city={city} />
+						))}
+					</div>
+				)}
 
-      </div>
-    </div>
-  );
+			</div>
+		</div>
+	);
 }
