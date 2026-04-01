0c95bf2abacfbebc6429784f5dfe7826122b7f0f
Da-vid-Lin <DavidLin771@outlook.com>
2026-04-01 01:52:26 +0100

    - Added symbols and units to display

--- src/components/Cards/DailyForecastCard.jsx
+++ src/components/Cards/DailyForecastCard.jsx
@@ -1,13 +1,20 @@
+// Displays the daily forecast for next 7 days 
+
+import { useSettings } from '../../contexts/settingsContext'
 import { formatDate } from '../../utils/formatDate'
+
 export default function DailyForecastCard({ dailyData }){
+    const { state } = useSettings()
+    const tempUnit = state.units === 'metric' ? '°C' : '°F'
+
     return(
         <div className="daily-forecast">
             {dailyData.map((day) => (
                 <div key={day.date}>
                     <p>{formatDate(day.date)}</p>
                     <img src={`https://openweathermap.org/img/wn/${day.icon}.png`} />
-                    <p>{((day.minTemp + day.maxTemp)/2).toFixed(0)}</p>
-                    <p>{day.chance}</p>
+                    <p>{day.temp.toFixed(0)}{tempUnit}</p>
+                    <p>🌧️{(day.chance*100).toFixed(0)}%</p>
                     
                 </div>
 
